a99d24048aefaa0d7e2ad650d7fc1e527658f459
Arkady Babulin <void@hq.dixy.ru>
2011-08-10 16:02:08 +0400

    Add initial functional

--- init.rb
+++ init.rb
@@ -4,15 +4,17 @@ require_dependency 'auto_assigned_user/hooks'
 
 Redmine::Plugin.register :redmine_auto_assigned_user do
   name 'Redmine Auto Assigned User plugin'
-  author 'Ludovic Gasc'
-  description 'If an user forget to assign the issue, this plugin will auto-assign to the project manager.'
+  author 'Arkady Babulin'
+  description 'If an user forget to assign the issue, this plugin will auto-assign to the project manager or CTO depends on tracker.'
   version '0.0.1'
-  url 'http://github.com/GMLudo/redmine_auto_assigned_user'
+  url 'http://github.com/the0retic/redmine_auto_assigned_user'
   author_url 'http://be.linkedin.com/in/ludovicgasc'
   requires_redmine :version_or_higher => '0.9.0'
 
   settings :default => {
+    'tracker_director' => '',
     'project_manager_role' => '',
+    'project_director_role' => '',
     'client_roles' => []
   }, :partial => 'settings/auto_assigned_user_settings'
 end
